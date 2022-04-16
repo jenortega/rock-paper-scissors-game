141361f6fab2953709520e89c9cfa116b7106c42
Jennefer Ortega <jennefereortega@gmail.com>
2022-04-16 11:57:24 -0400

    Complete UI

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # rock-paper-scissors-game
+>"Rock, Paper, Scissors Game" is a coding project advised by "The Odin Project". 
+
+## General Information
+The project started by writing the code in JavaScript to implementing graphic user interface.
+
+## Technologies
+* HTML5
+* CSS3
+* JavaScript
+
+## Status
+Complete.
+
+## Inspiration
+The Odin Project.
+
+## Contact
+Created by me. Please let me know if you have any questions. 
+
--- app.js
+++ app.js
@@ -1,97 +1,228 @@
-//Rock, Paper, Scissors Game
-
-//1. Greeting
-alert("Welcome to Rock, Paper, Scissors!");
-
 //To avoid misspelling
-let rock = "rock"
-let paper = "paper"
-let scissors = "scissors"
-let winner = ""
+let rock = "Rock";
+let paper = "Paper";
+let scissors = "Scissors";
 
-//Function for the game
+//Keep track of scores
+let playerScore = 0;
+let computerScore = 0;
+
+//Players selection
+let playerSelection = "";
+let computerSelection = "";
+      
+function selectOption() {
+  let num = Math.floor(Math.random() * 3);
+  let option = "";
+
+  if (num === 0) {
+    option = rock;
+  } else if (num === 1) {
+    option = paper;
+  } else if (num === 2) {
+    option = scissors;
+  }
+  return option;
+}
+
+let finalWinner = "";
+
+let round = 0;
+let roundLimit = 5;
+
+const startBtn = document.querySelector('.startButton');
+
+startBtn.addEventListener('click', () => {
+  const headerBox = document.getElementById('header');
+  
+  headerBox.removeChild(startBtn);
+
+  document.getElementById('game-area').scrollIntoView();
+
+  const headerText = document.querySelector('.title');
+  headerText.textContent = "Select your Object";
+
+  const viewRound = document.createElement('h4');
+  viewRound.classList.add('roundNumber');
+  viewRound.textContent = "round number: " + round;
+  
+  headerText.parentNode.insertBefore(viewRound, headerText.nextSibling);
+
+  const parent = document.querySelector('.playerTwo');
+  const child = document.createElement('div')
+  
+  child.classList.add('compPickBox')
+
+  const childText = document.createElement('span')
+  childText.setAttribute('id', 'compPickText')
+  childText.textContent = "Computer's Pick: ... "
+
+  child.appendChild(childText);
+  parent.appendChild(child);
+  
+  p1Buttons();
+});
+
+function p1Buttons(){
+  document.getElementById('buttonsAppear').innerHTML = 
+    "<button id='btn'>Rock</button><button id='btn'>Paper</button><button id='btn'>Scissors</button>";
+
+  const buttons = document.querySelectorAll('#btn');
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      playerSelection = button.innerHTML;
+      computerSelection = selectOption();
+
+      const computerPick = document.querySelector('#compPickText');
+      computerPick.classList.add('compPickText');
+      computerPick.textContent = "Computer's Pick: " + computerSelection;
+
+      round++;
+      const viewRoundNum = document.querySelector('.roundNumber');
+      viewRoundNum.textContent = "round number: " + round;
+      
+      if (round < roundLimit) {
+        game();
+      } else if (round === roundLimit) {
+        game();
+        finalScore();
+        playAgainFunc();
+        disableP1Buttons();
+      } 
+
+    });
+
+  });
+}
+
+//Round Winner
+function computerPlay(playerSelection, computerSelection) {
+  let results = "";
+
+  if (playerSelection === rock && computerSelection === scissors ||
+      playerSelection === scissors && computerSelection === paper ||
+      playerSelection === paper && computerSelection === rock) {
+    results = "Player Wins";
+  } else if (playerSelection === rock && computerSelection === paper ||
+    playerSelection === scissors && computerSelection === rock ||
+    playerSelection === paper && computerSelection === scissors) {
+    results = "Computer Wins";
+  } else {
+    results = "Tie";
+  }
+
+  return results;
+}
+
 function game() {
-    //2. User's input
-    let playerSelection = prompt("Pick your type - Rock, Paper, Scissors:").toLowerCase();
+  let roundWinner = computerPlay(playerSelection, computerSelection);
+  //console.log("playerSelection: " + playerSelection +  " --- computerSelection: " + computerSelection)
+  //console.log("round winner: " + roundWinner);
+  
+  if (roundWinner === "Player Wins") {
+    playerScore++;
+  } else if (roundWinner === "Computer Wins") {
+    computerScore++;
+  }
 
-    //3. Computer's input
-    function selectOption() {
-        let num = Math.floor(Math.random()*3);
-        let option = "";
-    
-        if (num === 0) {
-            option = rock;
-        } else if (num === 1) {
-            option = paper;
-        } else if (num === 2) {
-            option = scissors;
-        }
-    
-        return option;
-    }
+  document.getElementsByClassName('.roundNumber').textContent = round;
+  const p1 = document.querySelector('.p1-score');
+  p1.textContent = playerScore;
 
-    let computerSelection = selectOption(); 
+  const p2 = document.querySelector('.p2-score');
+  p2.textContent = computerScore;
 
-    //4. Pick the winner
-    function computerPlay(playerSelection, computerSelection) {
-        let results = ""
+  //console.log("playerScore: " + playerScore + " --- computerScore: " + computerScore)
+  //console.log("round: " + round)
 
+  const headerText = document.querySelector('.title');
 
-        if (playerSelection === rock && computerSelection === scissors) {
-            results = "Player Wins";
+  if (roundWinner === "Tie") {
+    headerText.textContent = roundWinner;   
+  } else {
+    headerText.textContent = roundWinner + " This Round"
+  }
+  
+}
 
-        } else if (playerSelection === scissors && computerSelection === paper) {
-            results = "Player Wins";
+function finalScore() {
+  if (playerScore > computerScore) {
+    finalWinner = "Player!";
+  } else if (playerScore < computerScore) {
+    finalWinner = "Computer!";
+  } else {
+    finalWinner = "Its a Tie! No winners!";
+  }
 
-        } else if (playerSelection === paper && computerSelection === rock) {
-            results = "Player Wins";
+  const container = document.querySelector('#game-area');
+  const content = document.createElement('div');
+  
+  content.classList.add('winnerBox');
+  content.setAttribute('id', 'finalWinner')
+  content.textContent = "Game Winner: " + finalWinner;
+  
+  container.parentNode.insertBefore(content, container.nextSibling)
 
-        } else if (playerSelection === rock && computerSelection === paper) {
-            results = "Computer Wins";
+  const lastContainer = document.querySelector('#lastContainer');
+  lastContainer.classList.add('lastContainer')
 
-        } else if (playerSelection === scissors && computerSelection === rock) {
-            results = "Computer Wins";
+  document.getElementById('lastContainer').innerHTML = 
+  '<button class="play-again"><a href="#players"></a>Play Again</a></button>';
 
-        } else if (playerSelection === paper && computerSelection === scissors) {
-            results = "Computer Wins";
-        } else {
-            results = "Tie";
-
-        }
-
-        return results;
-
-    }
-
-    winner = computerPlay(playerSelection, computerSelection)
-
-    //to see the code
-        //To see what the player picked 
-        console.log("playerSelection: " + playerSelection);
-        //To see what the computer picked
-        console.log("computerSelection:" + computerSelection);
-        //To see who won
-        console.log(winner)
-
-
-    return winner;
-
-
+  document.getElementById('lastContainer').scrollIntoView({ behavior: 'smooth', block: 'center' });
 
 }
 
-//5. keep track of score
-//and the end of the 5 round, declare the winner
+function playAgainFunc() {
+  const playAgain = document.querySelector('.play-again');
 
+  playAgain.addEventListener('click', () => {
+    const lastContainer = document.getElementById('lastContainer');
+    lastContainer.removeChild(playAgain);
 
-function playRound() {
+    const content = document.querySelector('.winnerBox');
+    content.classList.remove('winnerBox');
 
-    for (i = 0; i < 3; i++) {
-        console.log("i: " + i)
-        game();  
-    }
+    const finalWinnerBox = document.getElementById('finalWinner');
+    finalWinnerBox.textContent = '';
+    finalWinnerBox.removeAttribute('id');
+    
+    playerSelection = "";
+    computerSelection = "";
+        
+    playerScore = 0;
+    computerScore = 0;
+    
+    finalWinner = "";
+  
+    round = 0;
+  
+    const viewRoundNum = document.querySelector('.roundNumber');
+    viewRoundNum.textContent = "round number: " + round;
+    
+    const childText = document.querySelector('#compPickText');
+    childText.textContent = "Computer's Pick: ... "
+  
+    const p1BtnsBox = document.querySelector('#buttonsAppear')
+    p1BtnsBox.classList.toggle('disableButtons')
 
-}
+    const p1 = document.querySelector('.p1-score');
+    p1.textContent = playerScore;
 
-playRound();
+    const p2 = document.querySelector('.p2-score');
+    p2.textContent = computerScore;
 
+    const headerText = document.querySelector('.title');
+    headerText.textContent = "Select your Object";
+    
+    document.getElementById('game-area').scrollIntoView();
+
+  })
+
+};
+
+function disableP1Buttons () {
+  const p1BtnsBox = document.querySelector('#buttonsAppear')
+  p1BtnsBox.classList.toggle('disableButtons')
+};
